e5494be74c55c2e9faba754e20df670c9de2046c
WALTERSUEL <nick_pq_0@hotmail.com>
2022-11-30 00:06:57 -0500

    Subiendo ejercicios java

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+/*
+ Escribe un programa que muestre tu nombre por pantalla
+ */
+package ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Walter Nick Suel Arroyo"); //Con esto imprimimos en pantalla nuestro nombre
+    }
+}
---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+ Modifica el programa anterior para que además se muestre tu dirección y tu número de teléfono. 
+Asegúrate de que los datos se muestren en líneas separadas.
+ */
+package ejercicio2;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Nombre: Walter Nick Suel Arroyo");
+        System.out.println("Direccion: Los Olivos - Giron 259");
+        System.out.println("Telefono: 465 654 987");
+    }   
+}
